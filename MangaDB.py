e8b3d296fd127f39a37bc118c53a63d725721529
shenyue <kpmsantos31@gmail.com>
2020-05-07 12:42:49 +1000

    Initial Version

---
+++ MangaDB.py
@@ -0,0 +1,95 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import sqlite3
+
+root = Tk()
+root.title("MangaDB")
+root.geometry("400x400")
+
+#Actual database
+
+
+#table creation
+"""cDB.execute('''CREATE TABLE manga (
+        title text,
+        author text,
+        chapters integer
+        )''')"""
+
+
+#functions
+def submit():
+    #create or connect to database
+    connectDB = sqlite3.connect('manga_database.db')
+    #cursor instance
+    cDB = connectDB.cursor()
+    #insert to TABLE
+    cDB.execute("INSERT INTO manga VALUES (:Title, :Author, :Chapters)",
+            {
+                'Title': titleDB.get(),
+                'Author': authorDB.get(),
+                'Chapters': chaptersDB.get()
+            })
+    #commit changes to db
+    connectDB.commit()
+    #close conn
+    connectDB.close()
+    #clear text boxes
+    titleDB.delete(0, END)
+    authorDB.delete(0, END)
+    chaptersDB.delete(0, END)
+#query function
+def query():
+    #create or connect to database
+    connectDB = sqlite3.connect('manga_database.db')
+    #cursor instance
+    cDB = connectDB.cursor()
+
+    #query db
+    cDB.execute("SELECT *, oid FROM manga")
+    records = cDB.fetchall()
+    #loop thru results
+    print_records = ""
+    for record in records:
+        print_records += str(record) + "\n"
+
+    query_label = Label(root, text=print_records)
+    query_label.grid(row=5, column=0, columnspan=2)
+
+
+    #commit changes to db
+    connectDB.commit()
+    #close conn
+    connectDB.close()
+
+
+titleDB = Entry(root, width=30)
+titleDB.grid(row= 0, column=1, padx=20)
+
+authorDB = Entry(root, width=30)
+authorDB.grid(row= 1, column=1)
+
+chaptersDB = Entry(root, width=30)
+chaptersDB.grid(row= 2, column=1)
+
+#text box labels
+titleLbl = Label(root, text="Title")
+titleLbl.grid(row=0, column=0)
+
+authorLbl = Label(root, text="Author")
+authorLbl.grid(row=1, column=0)
+
+chaptersLbl= Label(root, text="Chapters")
+chaptersLbl.grid(row=2, column=0)
+
+#submit button
+submitBtn = Button(root, text="Add manga to database", command=submit)
+submitBtn.grid(row=3, columnspan=2, pady=10, padx=10, ipadx=100)
+
+#create query Button
+queryBtn = Button(root, text="Check manga", command=query)
+queryBtn.grid(row=4, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
+
+
+root.mainloop()
