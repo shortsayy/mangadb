e3ec8387ed1e96bf7bbeba817cca2d044e69f2eb
shenyue <kpmsantos31@gmail.com>
2020-08-24 08:58:17 +1000

    Have to redo everything, starting from scratch.

--- MangaDB.py
+++ MangaDB.py
@@ -74,42 +74,5 @@ def query():
     connectDB.commit()
     #close conn
     connectDB.close()
-
-
-titleDB = Entry(root, width=30)
-titleDB.grid(row= 0, column=1, padx=20, pady=(10, 0))
-
-authorDB = Entry(root, width=30)
-authorDB.grid(row= 1, column=1)
-
-chaptersDB = Entry(root, width=30)
-chaptersDB.grid(row= 2, column=1)
-
-delBox = Entry(root, width=30)
-delBox.grid(row=10, column=1)
-
-#text box labels
-titleLbl = Label(root, text="Title")
-titleLbl.grid(row=0, column=0, pady=(10, 0))
-
-authorLbl = Label(root, text="Author")
-authorLbl.grid(row=1, column=0)
-
-chaptersLbl= Label(root, text="Chapters")
-chaptersLbl.grid(row=2, column=0)
-
-delBoxLbl = Label(root, text="Delete Number")
-delBoxLbl.grid(row=10, column=0, pady=5)
-
-#submit button
-submitBtn = Button(root, text="Add manga to database", command=submit)
-submitBtn.grid(row=3, columnspan=2, pady=10, padx=10, ipadx=100)
-
-#create query Button
-queryBtn = Button(root, text="Check manga", command=query)
-queryBtn.grid(row=4, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
-
-#create delete button
-delBtn = Button(root, text="Delete manga", command=delete)
-delBtn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+ 
 root.mainloop()
