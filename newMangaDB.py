acc3f6d03ab37cd52ecbeb2151a772e7824804d6
shortsayy <shorts@users.noreply.github.com>
2020-08-31 10:55:40 +1000

    Deletion of parts of code
    Deleted heading lines 73-75 (anchor)
    -Deleted Column for treeview(because its sorted by mySQL already, which is a localhost based server I set up in order to do the database)
    
    MAIN PROBLEMS as of now:
    - MySQL workbench (the program used to do configure the dataase) won't open up, so that is being worked out at the moment.
    - The schema (a collection of tables, rows and a seperate query)

--- newMangaDB.py
+++ newMangaDB.py
@@ -14,23 +14,11 @@
 
 
 #main window for the  database
-from tkinter import  *
+from tkinter import *
 import tkinter as tk
-from tkinter import  ttk
-from tkinter import  messagebox
+from tkinter import ttk
+from tkinter import messagebox
 import mysql.connector
-
-
-
-
-root = Tk()
-root.title("Manga DB")
-root.geometry('400x400+100+20') # Starting size/position: width x height + leftDistance + topDistance
-root.minsize(200,200) #Minimum size: width, height
-
-#Frame mainwindow for the heading to be done.
-root.config()
-
 
 #mainwindow Label (the title on the top of the program)
 #mainLbl = tk.Label(root ,anchor='center' , background="#607D8B", compound='top', font=('Segoe UI Semibold', 16 ), foreground="#ffffff", justify='center' , text= 'MangaDB')
@@ -40,53 +28,55 @@ root.config()
 mdb = mysql.connector.connect(host="localhost",
                               user="shorts",
                               password="shorts",
-                              port=3306
+                              port=3306,
+                              database="mangadatabase"
 )
-cursor = mdb.cursor()
+cursor = mdb.cursor(buffered=True) #buffered cursor means that the database will fetch all rows
 cursor.execute("SHOW DATABASES")
-cc = print(cursor.execute())
 
-cDB = input("Would you like to create a database?: (y/n)")
-if cDB == 'y':
-    cursor.execute("SHOW DATABASES")
-    cursor
+root = Tk()
+root.title("Manga DB")
+root.geometry('400x400+100+20') # Starting size/position: width x height + leftDistance + topDistance
+root.minsize(200,200) #Minimum size: width, height
+
+#cursor.execute("CREATE DATABASE mangadatabase")
+#cc = print(cursor.execute())
+
+
+#if else statement for a function below(incomplete, prototype)
+#cDB = input("Would you like to create a database?: (y/n)")
+#if cDB == 'y':
+    #cursor.execute("SHOW DATABASES")
+   # cursor
+
 #function to ask user to create a database
-def create():
+#def create():
 
 #These frames are created for  the Database
 wrapperDB = LabelFrame(root, text="Manga Database")
 wrapperDB.pack(fill="both", expand= "yes")
-frmSch = Frame(root, text="Search")
+frmSch = Frame(root)
 frmSch.pack()
 frmEntries = Frame(root)
 frmEntries.pack()
 
 #Actual treeview
-DB =  ttk.Treeview(wrapperDB,  columns=(1,2,3,4), show="headings", height="6") #done to create basic treeview functions
-DB.column(0, width=270, minwidth=270, stretch=Tk.NO)
-DB.column(1, width=150, minwidth=150, stretch=Tk.NO)
-DB.column(2, width=400, minwidth=200)
-DB.column(3, width=80, minwidth=50, stretch=Tk.NO)
-DB.column(4, width=80, minwidth=50, stretch=Tk.NO)
-
+DB = ttk.Treeview(wrapperDB, columns=(1,2,3,4), show="headings", height="6") #done to create basic treeview functions
 #treeview heading, they' are for the columns
-DB.heading(0, text="ID", anchor=Tk.W)
-DB.heading(1, text="Author", anchor=Tk.W)
-DB.heading(2, text="Title", anchor=Tk.W)
-DB.heading(3, text="Chapter", anchor=Tk.W)
-DB.heading(4, text="Status", anchor=Tk.W)
-
+DB.heading(1, text="ID")
+DB.heading(2, text="Author")
+DB.heading(3, text="Title")
+DB.heading(4, text="Chapter")
+DB.pack()
 #functions
-def  update():
+def update():
     for i in rows:
         DB.insert(' ', 'end', values=i)
 
 #query for  mySQL
-query= "SELECT id, manga_author, manga_title, manga_chapter, manga_status"
+query= "SELECT ID, Author, Title, Chapter, Status"
 cursor.execute(query)
 rows = cursor.fetchall()
 update(rows)
-
-
 
 root.mainloop()
